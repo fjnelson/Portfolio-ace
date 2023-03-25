44024e2cc0b03729bbfe90f69d0173eddddbca01
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-03-25 13:46:09 -0700

    adding projects

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import meetioImage from '../images/meetio.JPG';
-import dosequisImage from '../images/dosequis.jpg';
-import portfolioImage from '../images/portfolio-website.JPG';
+import React from "react";
+import meetioImage from "../images/meetio.JPG";
+import dosequisImage from "../images/dosequis.jpg";
+import portfolioImage from "../images/portfolio-website.JPG";
+import { Button } from "reactstrap";
+import "./pages.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export default function Blog() {
   const projects = [
     {
-      title: 'Portfolio',
-      description: 'This website! My portfolio keeps growing as I gain more experience from live projects.',
+      title: "Portfolio",
+      description:
+        "This website! My portfolio keeps growing as I gain more experience from live projects.",
       imageUrl: portfolioImage,
-      projectUrl: 'https://github.com/fjnelson/Portfolio-ace',
-      hostedURL: 'https://fjnelson.github.io/Portfolio-ace/'
+      stack: "react css bootstrap fontAwesome ",
+      projectUrl: "https://github.com/fjnelson/Portfolio-ace",
+      hostedURL: "https://fjnelson.github.io/Portfolio-ace/",
     },
     {
-      title: 'Dosequis-API',
-      description: 'Dosequis-API uses Ticketmaster API and pulls weather data for the event from Open-Mateo API.',
+      title: "Dosequis-API",
+      description:
+        "Dosequis-API uses Ticketmaster API and pulls weather data for the event from Open-Mateo API.",
       imageUrl: dosequisImage,
-      projectUrl: 'https://github.com/fjnelson/01-project-dosequis-api',
-      hostedURL: 'https://fjnelson.github.io/01-project-dosequis-api/'
+      stack: "javascript jquery dayJS bulma",
+      projectUrl: "https://github.com/fjnelson/01-project-dosequis-api",
+      hostedURL: "https://fjnelson.github.io/01-project-dosequis-api/",
     },
     {
-      title: 'Meet-IO',
-      description: 'Meet-IO is an application that allows users to chat and schedule events with friends.',
+      title: "Meet-IO",
+      description:
+        "Meet-IO is an application that allows users to chat and schedule events with friends.",
       imageUrl: meetioImage,
-      projectUrl: 'https://github.com/fjnelson/Meet-IO',
-      hostedURL: 'https://meetio.herokuapp.com/'
+      stack: "handlebars express jquery socketIO express sequelize bcrypt bulma",
+      projectUrl: "https://github.com/fjnelson/Meet-IO",
+      hostedURL: "https://meetio.herokuapp.com/",
     },
   ];
 
@@ -34,12 +43,29 @@ export default function Blog() {
       <div className="card-container">
         {projects.map((project, index) => (
           <div key={index} className="card">
-            <img src={project.imageUrl} alt={project.title} className="card-image" />
+            <img
+              src={project.imageUrl}
+              alt={project.title}
+              className="card-image"
+            />
             <div className="card-content">
               <h2 className="card-title">{project.title}</h2>
               <p className="card-description">{project.description}</p>
-              <a href={project.projectUrl} className="card-link">View on Github</a>
-              <a href={project.hostedURL} className="card-link">Hosted Project</a>
+              <div className="stack-description">
+                {project.stack.split(" ").map((stack, index) => (
+                  <span key={index} className="stack">
+                    {stack}
+                  </span>
+                ))}
+              </div>
+              <div className="links">
+                <Button className="btn-light" href={project.hostedURL} target="_blank">
+                  <i className="fas fa-eye"></i> Hosted Project
+                </Button>
+                <Button className="btn-dark" href={project.projectUrl} target="_blank">
+                  <i className="fab fa-github"></i> View on Github
+                </Button>
+              </div>
             </div>
           </div>
         ))}
