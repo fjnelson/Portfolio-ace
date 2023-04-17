1883d75059fea1e6ec701b831d281c6246186d67
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-04-16 21:17:15 -0700

    adding react router and fixing css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
-import PortfolioContainer from "./components/PortfolioContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/pages/About";
+import Projects from "./components/pages/Blog";
+import Resume from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+// import PortfolioContainer from "./components/PortfolioContainer";
 import Footer from "./components/footer";
+import NavTabs from './components/NavTabs';
 
 function App() {
   return (
-    <div>
-      <PortfolioContainer />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div>
+      <NavTabs />
+        {/* <PortfolioContainer /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,18 +2,27 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
-function NavTabs({ currentPage, handlePageChange }) {
+function NavTabs() {
   return (
-    <Navbar className ="nav-tabs-lg" expand="lg" variant="dark">
+    <Navbar className="nav-tabs-lg" expand="lg" variant="dark">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" onClick={() => handlePageChange('Home')}>Home</Nav.Link>
-            <Nav.Link href="#about" onClick={() => handlePageChange('About')}>About Me</Nav.Link>
-            <Nav.Link href="#projects" onClick={() => handlePageChange('Projects')}>Projects</Nav.Link>
-            <Nav.Link href="#resume" onClick={() => handlePageChange('Resume')}>Resume</Nav.Link>
+            <Nav.Link as={Link} to="/" exact>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              About Me
+            </Nav.Link>
+            <Nav.Link as={Link} to="/projects">
+              Projects
+            </Nav.Link>
+            <Nav.Link as={Link} to="/resume">
+              Resume
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
