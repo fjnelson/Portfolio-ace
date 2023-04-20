7bc5b72a4f7334fafb9faa2e5b6ba96a260d46b6
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-04-19 20:11:22 -0700

    adjusting navtabs for exact true

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -11,19 +11,19 @@ function NavTabs() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" exact>
+            <Nav.Link as={Link} to="/" exact={true}>
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/about">
+            <Nav.Link as={Link} to="/about" exact={true}>
               About Me
             </Nav.Link>
-            <Nav.Link as={Link} to="/projects">
+            <Nav.Link as={Link} to="/projects" exact={true}>
               Projects
             </Nav.Link>
-            <Nav.Link as={Link} to="/resume">
+            <Nav.Link as={Link} to="/resume" exact={true}>
               Resume
             </Nav.Link>
-            <Nav.Link as={Link} to="/recommendations">
+            <Nav.Link as={Link} to="/recommendations" exact={true}>
               Recommendations
             </Nav.Link>
           </Nav>
