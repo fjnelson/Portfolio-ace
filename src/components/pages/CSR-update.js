6c406037996d898cc3d7d055349243752441db6f
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-05-14 10:05:24 -0700

    updating projects

--- src/components/pages/CSR-update.js
+++ src/components/pages/CSR-update.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./pages.css";
-import oldHome from '../images/CSR/old-home.JPG'
+import oldHome from "../images/CSR/old-home.JPG";
+import recommendations from "../images/CSR/recommendations.JPG";
+import oldNavbar from "../images/CSR/old-navbar.jpg";
 
 export default function CSR() {
   return (
-    <div>
-      <div className="CSR-container">
+    <div className="CSR-container">
+      <div className="CSR-container-title">
         <h1>Minor Web Update for CSR-jobs.com</h1>
       </div>
       <div>
@@ -16,8 +18,27 @@ export default function CSR() {
           already.
         </p>
       </div>
-      <div className='old-home-container'>
-      <img src= {oldHome} alt="old home page"></img>
+      <div className="old-home-container">
+        <img src={oldHome} alt="old home page"></img>
+      </div>
+      <p className="CSR-container-blog">
+        But I did notice a few things right off the bat that we needed to
+        update, critically. I put together a recommendations meeting and met
+        with the site owner to go over my findings.
+      </p>
+      <div className="recommendations-container">
+        <img src={recommendations} alt="old home page"></img>
+      </div>
+      <div>
+        <p className="CSR-container-blog">
+          Next, I got to work. I received documentation on how to access the
+          host, called uhost. I made myself familiar with the code and styling.
+          But, I wasn't familiar with some of the scripting. The codebase was using a
+          local application to set the navbar as well as what looked like HTML4.
+        </p>
+      </div>
+      <div className="old-navbar-container">
+        <img src={oldNavbar} alt="old nav bar"></img>
       </div>
     </div>
   );
