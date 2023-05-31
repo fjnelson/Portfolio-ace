0e2aa9ddcdba2be492d3d8ff99f411b417bbd74f
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-05-30 19:18:57 -0700

    updating about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './pages.css';
+import React from "react";
+import "./pages.css";
 
 export default function About() {
   return (
@@ -9,10 +9,26 @@ export default function About() {
       </div>
       <div>
         <p className="about-paragraph">
-        Web developer with a passion for creating innovative solutions drawing on an extensive background of Customer Success, IT Project Management, and IT Service Delivery experience in designing, developing, and deploying web applications. Earned a certificate in Full Stack Web Development from the University of Oregon Coding Boot Camp.  Project lead of Hello Stranger application, incorporating project Scrums and sprints.  The goal is to continue growing as a tech enthusiast and contribute to the success of dynamic, fast-paced, cross-functional development teams with a “Customer Champion” mentality.
+          <span class="it-styled">IT</span> enthusiast with a passion for
+          creating innovative solutions drawing on an extensive background of
+          Customer Success, IT Project Management, IT Service Delivery, and
+          Application Support experience in designing, developing, and deploying
+          web applications. Earned a certificate in Full Stack Web Development
+          from the University of Oregon Coding Boot Camp. <br /> <br />
+          <span class="it-styled">The</span> goal is to continue growing,
+          continue learning, and contribute to the success of dynamic,
+          fast-paced, cross-functional development teams with a “Customer
+          Champion” mentality. Effective individual contributor across strong
+          sets of teams. Strong communication skills, interfacing with both
+          technical and non-technical audiences. Fluent in front-end
+          technologies and an avid learner. Able to pick up new concepts
+          quickly.
+          <br /> <br />
+          <span class="it-styled">I</span> am a firm believer in "IT starts with
+          Customer Success" and live/work/breathe by that model.
+          <br /> <br />
         </p>
       </div>
     </div>
   );
 }
-
