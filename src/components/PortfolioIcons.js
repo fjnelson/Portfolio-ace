f2dc56bffbcadaa384625133c7768a72896ba2e7
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-06-10 18:01:18 -0700

    adding alt tags to main mage

--- src/components/PortfolioIcons.js
+++ src/components/PortfolioIcons.js
@@ -8,13 +8,13 @@ const Icons = ({ size }) => {
     <div className="icons-container">
       <div className="icons">
         <a href="https://www.linkedin.com/in/frankjohnnelson/" className="icon-link">
-          <FontAwesomeIcon icon={faLinkedinIn} size="3x" />
+          <FontAwesomeIcon icon={faLinkedinIn} size="3x" alt="LinkedIn" />
         </a>
         <a href="https://github.com/fjnelson" className="icon-link">
-          <FontAwesomeIcon icon={faGithub} size="3x" />
+          <FontAwesomeIcon icon={faGithub} size="3x" alt="GitHub"/>
         </a>
         <a href="https://stackoverflow.com/users/20086174/snuffle1upagus" className="icon-link">
-          <FontAwesomeIcon icon={faStackOverflow} size="3x" />
+          <FontAwesomeIcon icon={faStackOverflow} size="3x" alt="Stack Overflow"/>
         </a>
       </div>
     </div>
